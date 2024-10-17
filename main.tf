8abc7d9d477e15cc8ff88fbac1d447110e22f6de
pranay patil <pranay7505@gmail.com>
2024-10-17 13:03:39 +0530

    add variables into v.tf folder

--- main.tf
+++ main.tf
@@ -11,12 +11,13 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-   subscription_id = "72c85e04-6d5a-484d-a0ce-325cdfd9fd10"
-   client_id = "3e291224-db59-4c21-8b51-0f1468f2510c"
-   client_secret = "sm68Q~ywN2XG-~t6tF7fFt3crB2lklIQoOB9DcgV"
-   tenant_id = "a1102ec6-f684-4618-84be-d1586d3ecc65"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
   features {}
 }
+
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
