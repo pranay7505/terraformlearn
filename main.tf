11f187d15bd8010c0529565c61203a65257b6faa
pranay patil <pranay7505@gmail.com>
2024-10-17 13:17:39 +0530

    adding new stuff into the main.tf

--- main.tf
+++ main.tf
@@ -25,3 +25,12 @@ resource "azurerm_resource_group" "example" {
   location = "Central India"
 }
 
+resource "azurerm_storage_account" "example" {
+  name                     = "storme"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+
+}
