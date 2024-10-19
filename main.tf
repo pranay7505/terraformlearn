58a171025212cec6d487fdf90fa7ff91d2a207f4
pranay patil <pranay7505@gmail.com>
2024-10-19 12:44:04 +0530

    adding new main.tf

--- main.tf
+++ main.tf
@@ -7,13 +7,7 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  subscription_id = "72c85e04-6d5a-484d-a0ce-325cdfd9fd10"
-  client_id       = "3e291224-db59-4c21-8b51-0f1468f2510c"
-  client_secret   = "sm68Q~ywN2XG-~t6tF7fFt3crB2lklIQoOB9DcgV"
-  tenant_id       = "a1102ec6-f684-4618-84be-d1586d3ecc65"
-  features {}
-}
+
 
 resource "azurerm_resource_group" "app_resource" {
   name     = "app-resource"
