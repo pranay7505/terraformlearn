f18a7f17a8bca09e71ab6f3b4e0b4b35a1cc6d07
pranay patil <pranay7505@gmail.com>
2024-10-19 12:40:51 +0530

    adding new main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,92 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      source = "hashicorp/azurerm"
+      version = "2.93.0"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  subscription_id = "72c85e04-6d5a-484d-a0ce-325cdfd9fd10"
+  client_id       = "3e291224-db59-4c21-8b51-0f1468f2510c"
+  client_secret   = "sm68Q~ywN2XG-~t6tF7fFt3crB2lklIQoOB9DcgV"
+  tenant_id       = "a1102ec6-f684-4618-84be-d1586d3ecc65"
   features {}
 }
 
-
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "pranay_resorces"
-  location = "Central India"
+resource "azurerm_resource_group" "app_resource" {
+  name     = "app-resource"
+  location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "storme"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
 
 
+resource "azurerm_virtual_network" "app_network" {
+  name                = "example-network"
+  location            = azurerm_resource_group.app_resource.location
+  resource_group_name = azurerm_resource_group.app_resource.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name             = "SubnetA"
+    address_prefixes = "10.0.1.0/24"
+
+  }
+
+}
+
+resource "azurerm_subnet" "app_subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.app_resource.name
+  virtual_network_name = azurerm_virtual_network.app_network.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "app_interface" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.app_resource.location
+  resource_group_name = azurerm_resource_group.app_resource.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.app_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.app_public.id
+  }
+  depends_on = [ azurerm_subnet.app_subnet ]
+}
+
+resource "azurerm_windows_virtual_machine" "app_machine" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.app_resource.name
+  location            = azurerm_resource_group.app_resource.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.app_interface.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+resource "azurerm_public_ip" "app_public" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = azurerm_resource_group.app_resource.name
+  location            = azurerm_resource_group.app_resource.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
 }
---
+++ main01.tf
@@ -0,0 +1,36 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  features {}
+}
+
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "pranay_resorces"
+  location = "Central India"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storme"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+
+}
